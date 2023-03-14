c7c2a21837b13c6647c8df25386ccc96fd764df9
aburakuslu <alpburakuslu@gmail.com>
2023-03-14 23:14:55 +0300

    day_03

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void firstTest01() {
+
+    }
+
+    @Test
+    public void firstTest02() {
+
+    }
+
+    @Test
+    public void firstTest03() {
+
+    }
+
+}
